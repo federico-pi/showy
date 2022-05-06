e7eb0bb1762009861fab8005eb8d20a44fd85a7e
Federico Balzi <balzi.federico@gmail.com>
2022-05-06 17:10:29 +0200

    build(app): Configuration files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          '~api': './src/api',
+          '~assets': './src/assets',
+          '~core': './src/core',
+          '~helpers': './src/helpers',
+          '~hooks': './src/hooks',
+          '~mocks': './src/mocks',
+          '~shared': './src/shared',
+          '~stores': './src/stores',
+          '~styles': './src/styles',
+        },
+      },
+    ],
+    [
+      require.resolve('react-native-dotenv'),
+      {
+        moduleName: '~env',
+        path: 'environments/.env',
+      },
+    ],
+  ],
 };
