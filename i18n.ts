964116317888c5c187685e592887dcfdf1cea9d8
Federico Balzi <balzi.federico@gmail.com>
2022-05-07 03:08:17 +0200

    refactor(i18n): Improvement

--- i18n.ts
+++ i18n.ts
@@ -2,11 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import commons from '~assets/i18n/en/commons.json';
+import home from '~assets/i18n/en/home.json';
 import misc from '~assets/i18n/en/misc.json';
 
 export const resources = {
   en: {
     commons,
+    home,
     misc,
   },
 } as const;
@@ -14,7 +16,7 @@ export const resources = {
 i18n.use(initReactI18next).init({
   compatibilityJSON: 'v3',
   lng: 'en',
-  ns: ['commons', 'misc'],
+  ns: ['commons', 'home', 'misc'],
   resources,
 });
 
