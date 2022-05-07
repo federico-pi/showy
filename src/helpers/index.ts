a8b42104b3de182b14795a5eab2e93dca836bf62
Federico Balzi <balzi.federico@gmail.com>
2022-05-07 03:09:00 +0200

    feat(helpers): Setup

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+import * as AppHelpers from './app';
+import * as PermissionsHelpers from './permissions';
+import * as SharedHelpers from './shared';
+
+export { AppHelpers, PermissionsHelpers, SharedHelpers };
---
+++ src/helpers/shared.ts
@@ -0,0 +1,31 @@
+/**
+ * Helper to capitalize the first letter
+ * @param string - The string to capitalize
+ * @param locale - The current locale
+ */
+export function capitalizeFirstLetter(
+  [first, ...rest]: string,
+  locale: string = 'it'
+): string {
+  return `${first.toLocaleUpperCase(locale)}${rest.join('')}`;
+}
+
+/**
+ * Helper to capitalize the first letter of each word in a sentence
+ */
+export function capitalizeWords(sentence: string): string {
+  return sentence
+    .toLowerCase()
+    .split(' ')
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    .join(' ');
+}
+
+/**
+ * Generic key extractor
+ */
+export function keyExtractor() {
+  return (item: unknown, index: number): string => index.toString();
+}
